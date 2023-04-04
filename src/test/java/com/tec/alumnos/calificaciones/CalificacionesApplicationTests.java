ce464dbc72d293e5243b2567bc16db45debf9bb1
omararguellesgar <120700188+omararguellesgar@users.noreply.github.com>
2023-04-04 12:06:05 -0600

    commit fix

---
+++ src/test/java/com/tec/alumnos/calificaciones/CalificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tec.alumnos.calificaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
